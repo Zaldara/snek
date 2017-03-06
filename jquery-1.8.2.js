f1094b3865e0edc74028d1e377ce1272e2d79265
Weston S <weston5939@gmail.com>
2017-03-06 10:05:33 -0500

    Update jquery-1.8.2.js

--- jquery-1.8.2.js
+++ jquery-1.8.2.js
@@ -7,7 +7,6 @@
  *
  * Copyright 2012 jQuery Foundation and other contributors
  * Released under the MIT license
- * http://jquery.org/license
  *
  * Date: Thu Sep 20 2012 21:13:05 GMT-0400 (Eastern Daylight Time)
  */
